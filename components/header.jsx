2f468ed23a9b6716ce758fd556a7e3d093f82c50
aryan-srivastava1625 <ap9140@srmist.edu.in>
2025-04-26 20:01:47 +0530

    making the dashboard

--- components/header.jsx
+++ components/header.jsx
@@ -1,9 +1,46 @@
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { Button } from './ui/button'
+import { LayoutDashboard } from 'lucide-react'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu'
 
 const Header = () => {
   return (
-    <div>
+    <header className='fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60'>
+        
+        <nav className='container mx-auto px-4 h-16 flex items-center justify-between '>
+            <Link href="/">
+                <Image src="/logo.png" alt="Coach Logo" width={200} height={60} className="h-12 py-1 w-auto object-contain"/>
+            </Link>
+        
+
+        <div>
+            <SignedIn>
+                <Link href={"/dashboard"}>
+                    <Button>
+                        <LayoutDashboard className=" h-4 w-4" />
+                        <span className='hidden md:block'>Industry Insights</span>
+                        
+                    </Button>
+                </Link>
+            </SignedIn>
+            <DropdownMenu>
+                <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>Profile</DropdownMenuItem>
+                    <DropdownMenuItem>Billing</DropdownMenuItem>
+                    <DropdownMenuItem>Team</DropdownMenuItem>
+                    <DropdownMenuItem>Subscription</DropdownMenuItem>
+                </DropdownMenuContent>
+                </DropdownMenu>
+        </div>
+        </nav>
+
+
        <SignedOut>
               <SignInButton />
               
@@ -11,8 +48,9 @@ const Header = () => {
             <SignedIn>
               <UserButton />
             </SignedIn>
-    </div>
-  )
+    </header>
+  );
 }
 
 export default Header
+ 
